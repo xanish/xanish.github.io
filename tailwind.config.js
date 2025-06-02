4b9af17945ef5a2b92b1329e92427bf25cc42631
xanish <danishali7866@gmail.com>
2025-06-03 00:06:00 +0530

    update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./content/**/*.md', './layouts/**/*.html', './assets/**/*.css'],
+  content: ["./content/**/*.md", "./layouts/**/*.html", "./assets/**/*.css"],
   theme: {
     extend: {
       fontFamily: {
-        body: ['Inter', 'sans-serif'],
-        heading: ['Montserrat', 'sans-serif'],
+        body: [
+          "Atkinson Hyperlegible Next",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        heading: [
+          "Bitter",
+          "ui-serif",
+          "Georgia",
+          "Cambria",
+          "Times New Roman",
+          "Times",
+          "serif",
+        ],
+        mono: [
+          "JetBrains Mono",
+          "ui-monospace",
+          "SFMono-Regular",
+          "Menlo",
+          "Monaco",
+          "Consolas",
+          "Liberation Mono",
+          "Courier New",
+          "monospace",
+        ],
       },
     },
   },
   plugins: [],
-}
+};
